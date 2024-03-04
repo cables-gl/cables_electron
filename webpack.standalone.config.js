f3fa854253343fe27f8f504d0ef28daff25d37d1
Stephan Maihöfer <stephan@undev.de>
2024-03-04 17:58:30 +0100

    no resolve...

--- webpack.standalone.config.js
+++ webpack.standalone.config.js
@@ -4,12 +4,13 @@ import webpack from "webpack";
 export default (isLiveBuild, buildInfo) =>
 {
     const __dirname = new URL(".", import.meta.url).pathname;
+    console.log("DIRNAME", __dirname);
     return {
         "context": __dirname,
         "mode": isLiveBuild ? "production" : "development",
         "devtool": isLiveBuild ? "source-map" : "eval-cheap-module-source-map",
         "entry": {
-            "scripts.standalone.js": [path.resolve(path.join(__dirname, "src_client", "index_standalone.js"))]
+            "scripts.standalone.js": [path.join(__dirname, "src_client", "index_standalone.js")]
         },
         "output": {
             "path": path.resolve(path.join(__dirname, "public", "js")),
