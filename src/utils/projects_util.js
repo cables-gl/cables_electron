ce567e63621fbc8f9bccbcc60f03fdc2ef52c9f8
Stephan Maihöfer <stephan@undev.de>
2024-03-05 12:25:43 +0100

    build mac

--- src/utils/projects_util.js
+++ src/utils/projects_util.js
@@ -12,20 +12,5 @@ class ProjectsUtil extends SharedProjectsUtil
     {
         return this.getAssetPath(pId) + "/_screenshots/";
     }
-
-    /**
-     * do not convert screenshots on standalone version
-     *
-     * @param project
-     * @param filenameScreenshot
-     * @param ext
-     * @param assetPath
-     * @private
-     * @return boolean
-     */
-    _convertScreenShot(project, filenameScreenshot, ext, assetPath)
-    {
-        return true;
-    }
 }
 export default new ProjectsUtil(utilProvider);
