e27da8e8052679c073a9b971e2b944156d3ec205
Stephan Maihöfer <stephan@undev.de>
2024-05-07 17:51:31 +0200

    notarize?

---
+++ resources/build/notarize.js
@@ -0,0 +1,21 @@
+import { notarize } from "electron-notarize";
+import { fileURLToPath } from "url";
+import path from "path";
+
+export default async function notarizing(context)
+{
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== "darwin")
+    {
+        return;
+    }
+
+    return await notarize({
+        "tool": "notarytool",
+        "appBundleId": "gl.cables.standalone",
+        "appPath": appOutDir,
+        "appleId": process.env.APPLE_ID,
+        "appleIdPassword": process.env.APPLE_APP_SPECIFIC_PASS,
+        "teamId": process.env.APPLE_TEAM_ID
+    });
+}
