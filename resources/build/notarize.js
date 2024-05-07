0b71146fa83778adabe30fc634d158af00940944
Stephan Maihöfer <stephan@undev.de>
2024-05-07 18:05:35 +0200

    envvar

--- resources/build/notarize.js
+++ resources/build/notarize.js
@@ -15,7 +15,7 @@ export default async function notarizing(context)
         "appBundleId": "gl.cables.standalone",
         "appPath": appOutDir,
         "appleId": process.env.APPLE_ID,
-        "appleIdPassword": process.env.APPLE_APP_SPECIFIC_PASS,
+        "appleIdPassword": process.env.APPLE_APP_SPECIFIC_PASSWORD,
         "teamId": process.env.APPLE_TEAM_ID
     });
 }
