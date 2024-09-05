51d265fb21f473d4ba022995dbb667e4fcd82350
Stephan Maihöfer <stephan@undev.de>
2024-09-05 17:49:12 +0200

    use username from editorpreferences if set

--- src/electron/electron_settings.js
+++ src/electron/electron_settings.js
@@ -130,7 +130,7 @@ class ElectronSettings
 
     getCurrentUser()
     {
-        let username = "";
+        let username = this.getUserSetting("authorName", "");
         return {
             "username": username,
             "_id": helper.generateRandomId(),
