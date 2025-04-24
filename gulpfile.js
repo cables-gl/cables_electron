1ec3b377f307147409e403835ab382dcf1c1b1a2
Stephan Maihöfer <stephan@undev.de>
2025-04-24 17:36:36 +0200

    watch less

--- gulpfile.js
+++ gulpfile.js
@@ -50,9 +50,9 @@ const minify = config.hasOwnProperty("minifyJs") ? config.minifyJs : false;
 const watchers = [];
 function _watch(done)
 {
-    const watchOptions = { "usePolling": true, "ignored": (fileName) => { return fileName.includes("node_modules"); } };
-    watchers.push(gulp.watch(["src_client/*.js", "src_client/**/*.js", "../shared/shared_constants.json", "../shared/client/**/*.js"], watchOptions, gulp.series(defaultSeries)));
-    watchers.push(gulp.watch(["src/*.js", "src/**/*.js", "../shared/api/**/*.js"], watchOptions, gulp.series(electronChanges)));
+    const watchOptions = { "ignored": "./**/node_modules/" };
+    watchers.push(gulp.watch(["src_client/**/*.js", "../shared/shared_constants.json", "../shared/client/**/*.js"], watchOptions, gulp.series(defaultSeries)));
+    watchers.push(gulp.watch(["src/**/*.js", "../shared/api/**/*.js"], watchOptions, gulp.series(electronChanges)));
     done();
 }
 
